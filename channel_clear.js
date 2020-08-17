67233ea98b9dc0859d1513a5906908fbb6e9e650
Legit Dongo <legitdongo@gmail.com>
2020-08-17 17:54:39 -0500

    Track timezone per channel given (or just the default)

--- channel_clear.js
+++ channel_clear.js
@@ -3,10 +3,15 @@ let fs = require('fs');
 let client = new discord.Client();
 let folderRoot = __dirname + '/';
 let config = JSON.parse(fs.readFileSync(folderRoot + 'config.ini', 'utf8'));
+let { DateTime } = require('luxon');
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
-    client.setTimeout(clean_channel, get_midnight_tonight());
+    for(let i in config.channels) {
+        let id = typeof config.channels[i] === 'object' ? config.channels[i].id : config.channels[i];
+        let timezone = typeof config.channels[i] === 'object' ? config.channels[i].timezone : null;
+        client.setTimeout(clean_channel, get_midnight_tonight(timezone), id, timezone);
+    }
 });
 
 let delete_messages = async (channel) => {
@@ -17,22 +22,20 @@ let delete_messages = async (channel) => {
     await channel.bulkDelete(messages);
 };
 
-let get_midnight_tonight = () => {
-    var now = new Date();
-    var millisTillMidnight = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 24, 0, 0, 0) - now;
+let get_midnight_tonight = (timezone = null) => {
+    let now = DateTime.local().setZone(timezone).toMillis();
+    let millisTillMidnight = DateTime.fromObject({hour: 24, zone: timezone}).toMillis() - now;
     if (millisTillMidnight < 0) {
         millisTillMidnight += 86400000; // it's after 24:00, try 24:00 tomorrow.
     }
     return millisTillMidnight;
 };
 
-let clean_channel = () => {
-    for(let i = 0; i < config.channels.length; i++) {
-        client.channels.fetch(config.channels[i])
-        .then(channel => delete_messages(channel))
-        .catch(console.error);
-    }
-    client.setTimeout(clean_channel, get_midnight_tonight());
+let clean_channel = (channel_id, timezone) => {
+    client.channels.fetch(channel_id)
+    .then(channel => delete_messages(channel))
+    .catch(console.error);
+    client.setTimeout(clean_channel, get_midnight_tonight(timezone), channel_id, timezone);
 };
   
 client.login(config.token);
